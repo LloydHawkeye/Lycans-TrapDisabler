1ab52e0484897f8a2025148d71dd998a0b24e09e
LloydHawkeye <nicolas.lange33@gmail.com>
2024-04-15 20:33:50 +0200

    Release v1.0.0

---
+++ TrapDisabler/Patch/GameManagerPatch.cs
@@ -0,0 +1,47 @@
+﻿using Fusion;
+using Helpers.Collections;
+using JetBrains.Annotations;
+using Managers;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static GameState;
+
+namespace TrapDisabler.Patch
+{
+    internal class GameManagerPatch
+    {
+        public static void Patch()
+        {
+            On.GameManager.SpawnRandomItems += GameManager_SpawnRandomItems;
+        }
+
+        private static void GameManager_SpawnRandomItems(On.GameManager.orig_SpawnRandomItems orig, GameManager self)
+        {
+            if (GameManager.Instance.HuntersCount > 0)
+            {
+                for (int i = 0; i < self.ItemsSpawnRate * 3; i++)
+                {
+                    Transform andLockRandomItemSpawn = self.GetAndLockRandomItemSpawn();
+                    if (andLockRandomItemSpawn != null)
+                    {
+                        self.Runner.Spawn(self.bulletPrefab, andLockRandomItemSpawn.position, andLockRandomItemSpawn.rotation);
+                    }
+                }
+            }
+            if (!self.BattleRoyale)
+            {
+                return;
+            }
+            int num = PlayerRegistry.Where((PlayerController p) => !p.IsDead).Count();
+            for (int k = 0; k < num * 10; k++)
+            {
+                Transform andLockRandomItemSpawn3 = self.GetAndLockRandomItemSpawn();
+                if (andLockRandomItemSpawn3 != null)
+                {
+                    self.Runner.Spawn(self.bulletPrefab, andLockRandomItemSpawn3.position, andLockRandomItemSpawn3.rotation);
+                }
+            }
+        }
+    }
+}
---
+++ TrapDisabler/TrapDisabler.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using TrapDisabler.Patch;
+using UnityEngine;
+
+namespace TrapDisabler
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("Lycans.exe")]
+    public class TrapDisabler : BaseUnityPlugin
+    {
+        // Le GUID du plugin doit être unique au plugin que vous créez
+        // Il doit également être lisible car est utilisé dans le nom des fichiers de config
+        // Changez le nom d'auteur et le nom du plugin afin que le GUID soit correctement
+        // initialisé pour votre mod
+        public const string PLUGIN_GUID = $"{PLUGIN_AUTHOR}.{PLUGIN_NAME}";
+        public const string PLUGIN_AUTHOR = "LloydHawkeye";
+        public const string PLUGIN_NAME = "TrapDisabler";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            // Initialise le logger global du plugin
+            Log.Init(Logger);
+            GameManagerPatch.Patch();
+        }
+    }
+}
